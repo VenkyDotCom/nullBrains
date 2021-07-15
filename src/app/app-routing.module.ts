e3679012637d57701595bdf64f88fd8ead6e15ee
venkie <venkytech6@gmail.com>
2021-07-15 08:24:30 +0530

    inital commit for nullBrains

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FeaturesComponent } from './features/features.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'}),
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: DashboardComponent},
+      {path: 'features', component: FeaturesComponent},
+      {path: 'blog', component: BlogComponent},
+      {path: 'contact', component: ContactComponent},
+      {path: '', redirectTo: '/home', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ]),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
